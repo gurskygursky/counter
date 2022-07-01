4b364ebcacaa4059441cf6a347b6286ebd4fcf49
yegorgursky <yahorhurski@gmail.com>
2022-07-01 16:00:31 +0300

    add logic

--- src/PrimitiveCounter/PrimitiveCounter.tsx
+++ src/PrimitiveCounter/PrimitiveCounter.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './PrimitiveCounter.module.css';
 
 export const PrimitiveCounter = () => {
+    const [counterValue, setCounterValue] = useState<number>(0);
+
+    const increaseCounterValue = () => {
+        setCounterValue(counterValue + 1);
+    }
+    const resetCounterValue = () => {
+        setCounterValue(0);
+    }
+
+
     return (
         <div className={style.counterWrapper}>
-            <div className={style.outputWrapper}>{}</div>
+            <div className={style.outputWrapper}>{counterValue}</div>
             <div className={style.buttonsWrapper}>
-                <button className={style.bttn}>INC</button>
-                <button className={style.bttn}>RESET</button>
+                <button className={style.bttn} onClick={increaseCounterValue} disabled={counterValue === 10}>INC</button>
+                <button className={style.bttn} onClick={resetCounterValue} disabled={counterValue === 0}>RESET</button>
             </div>
         </div>
     );
