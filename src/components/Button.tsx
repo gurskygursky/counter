5c095b3aa24af06fd54deca4c8076b9cce4a7cc8
yegorgursky <yahorhurski@gmail.com>
2022-07-03 22:53:57 +0300

    add custom button and input components

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type ButtonPropsType = {
+    buttonTitle: string;
+    callback: () => void;
+    className?: string;
+    disabled?: boolean;
+}
+
+export const Button = (props: ButtonPropsType) => {
+
+    const onClickButtonHandler = () => {
+        props.callback();
+    }
+    return (
+        <button onClick={onClickButtonHandler} className={props.className}>{props.buttonTitle}</button>
+    );
+};
